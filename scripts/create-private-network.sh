1a99682c8e3748c6edf79c3857daa873283ded0e
Gert Drapers <gert.drapers@live.com>
2015-08-05 22:06:02 -0700

    Update create-private-network.sh
    Update naming to match kubesetup.yml and HP public cloud default external network name Ext-Net

--- scripts/create-private-network.sh
+++ scripts/create-private-network.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
 
-EXTERNAL_NETWORK_NAME="external"
+# execute neutron net-external-list to determin value
+EXTERNAL_NETWORK_NAME="Ext-Net"
 
-neutron net-create als-network
-neutron subnet-create --name als-subnet --allocation-pool start=192.168.15.10,end=192.168.15.250 --dns-nameserver 206.164.176.23 als-network 192.168.15.0/24
-neutron router-create als-router
-neutron router-gateway-set als-router ${EXTERNAL_NETWORK_NAME}
-neutron router-interface-add als-router als-subnet
+neutron net-create kube-net
+neutron subnet-create --name kube-subnet --allocation-pool start=192.168.1.100,end=192.168.1.200 kube-net 192.168.1.0/24
+neutron router-create kube-router
+neutron router-gateway-set kube-router ${EXTERNAL_NETWORK_NAME}
+neutron router-interface-add kube-router kube-subnet
